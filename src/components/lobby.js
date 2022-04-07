bc87cec585fb0452fd23907b4e279e3b682978d7
tahmdev <qwertiimusic@gmail.com>
2022-04-07 19:35:29 +0200

    Added leave functionality

--- src/components/lobby.js
+++ src/components/lobby.js
@@ -3,23 +3,25 @@ import { useEffect, useState } from 'react';
 import {useParams} from "react-router-dom";
 import RoomSettings from "./RoomSettings";
 
-const Lobby = ({socket, apiUrl, id}) => {
+const Lobby = ({socket, id, room, setRoom}) => {
+  let {roomID} = useParams();
   const [stairs, setStairs] = useState(["ロード中"])
-  const [room, setRoom] = useState("dailyKanji")
   const [roomInfo, setRoomInfo] = useState()
  
   // get room on route change
-  let { roomID } = useParams();
   useEffect(() => {
+    console.log(room)
+    console.log("LEAVING NOW", roomID)
+    socket.emit("leaveRoom", room)
     if (roomID) {
       setRoom(roomID)
 
     //joins room based on URL OR join dailyKanji
-      socket.emit("joinRoom", roomID)
+      socket.emit("joinRoom", {roomID: roomID, playerName: "name 2"})
     }else{
-      socket.emit("joinRoom", room)
+      socket.emit("joinRoom", {roomID: room, playerName: "name here"})
     }
-  }, [roomID])
+  }, [roomID]) 
 
   // update stairs and room info via websocket
   useEffect(() => {
@@ -29,14 +31,14 @@ const Lobby = ({socket, apiUrl, id}) => {
     socket.on("newRoomInfo", data => {
       setRoomInfo(data)
       setStairs([...data.stairs])
-      console.log(data)
     })
   }, []);
 
   return(
     <div key={id} >
       <button onClick={() => console.log(roomInfo)} > Log roomInfo</button>
-      {!roomInfo && <div> aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa </div>}
+      <button onClick={() => console.log(room)} > Log CURRENT ROOM</button>
+      {!roomInfo && <div> This lobby does not exist </div>}
       {roomInfo && roomInfo.status === "active" && <StairDisplay roomInfo={roomInfo} stairs={stairs} room={room} socket={socket} />}
       {roomInfo && roomInfo.status === "waiting" && <RoomSettings socket={socket} roomInfo={roomInfo} roomID={roomID} />}
     </div>
